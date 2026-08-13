d4e363169af93baafc2fa9d99cc7d5f9e36a29f9
Ch-Kumar-Kartik <kartik.kumar.chintakrindi@gmail.com>
2026-08-13 20:29:56 +0530

    added auth

--- routers/users.py
+++ routers/users.py
@@ -7,18 +7,25 @@ from sqlalchemy.orm import selectinload
 
 import models
 from database import get_db
-from schemas import PostResponse, UserCreate, UsserResponse, UserUpdate
+from schemas import UserCreate, UserPublic, UserPrivate, UserUpdate, Token, PostResponses
+
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
+
+from sqlalchemy import func
+from config import settings
+from auth import create_access_token, hash_password, oauth2_scheme, password_hash, verify_access_token, verify_password
 
 router = APIRouter()
 
 @router.post(
-    "/api/users",
-    response_model=UserResponse,
+    "",
+    response_model=UserPrivate,
     status_code=status.HTTP_201_CREATED,
 )
 async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
     result = await db.execute(
-        select(models.User).where(models.User.username == user.username),
+        select(models.User).where(func.lower(models.User.username) == user.username.lower()),
     )
     existing_user = result.scalars().first()
     if existing_user:
@@ -28,7 +35,7 @@ async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_
         )
 
     result = await db.execute(
-        select(models.User).where(models.User.email == user.email),
+        select(models.User).where(func.lower(models.User.email) == user.email.lower()),
     )
     existing_email = result.scalars().first()
     if existing_email:
@@ -39,9 +46,124 @@ async def create_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_
 
     new_user = models.User(
         username=user.username,
-        email=user.email,
+        email=user.email.lower(),
+        password_hash = password_hash(user.password),
     )
     db.add(new_user)
     await db.commit()
     await db.refresh(new_user)
     return new_user
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+
+    result = await db.execute(select(models.User).where(func.lower(models.User.email) == form_data.username.lower()))
+    user = result.scalars().fist()
+
+    if not user or not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            details="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+    access_token = create_access_token(
+        data = {"sub": str(user.id)},
+        expires_delta = access_token_expires,
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
+
+@router.get("/me", response_model = UserPrivate)
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Annotated[AsyncSession, Depends(get_db)]):
+    pass
+    
+@router.get("/{user_id}", response_model=UserResponse)
+async def get_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if user:
+        return user
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+
+@router.get("/{user_id}/posts", response_model=list[PostResponse])
+async def get_user_posts(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    result = await db.execute(
+        select(models.Post)
+        .options(selectinload(models.Post.author))
+        .where(models.Post.user_id == user_id),
+    )
+    posts = result.scalars().all()
+    return posts
+
+
+@router.patch("/{user_id}", response_model=UserResponse)
+async def update_user(
+    user_id: int,
+    user_update: UserUpdate,
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    if user_update.username is not None and user_update.username != user.username:
+        result = await db.execute(
+            select(models.User).where(models.User.username == user_update.username),
+        )
+        existing_user = result.scalars().first()
+        if existing_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Username already exists",
+            )
+    if user_update.email is not None and user_update.email != user.email:
+        result = await db.execute(
+            select(models.User).where(models.User.email == user_update.email),
+        )
+        existing_email = result.scalars().first()
+        if existing_email:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered",
+            )
+
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.email is not None:
+        user.email = user_update.email
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+
+    await db.commit()
+    await db.refresh(user)
+    return user
+
+
+@router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+
+    await db.delete(user)
+    await db.commit()
