2b4d02773007addf8de0e9532616e3dca282e6d2
Ch-Kumar-Kartik <kartik.kumar.chintakrindi@gmail.com>
2026-09-05 21:54:34 +0530

    added password reset and background tasks

--- email_utils.py
+++ email_utils.py
@@ -32,5 +32,32 @@ async def send_email(
         start_tls=settings.mail_use_tls,)
 
 async def send_password_reset_email(to_email:str, username:str, reset_token:str) -> None:
+    reset_url = f"{settings.frontend_url}/reset-password?token={reset_token}"
+    template = templates.get_template("email/password_reset.html")
+    html_content = template.render(reset_url=reset_url, username=username)
+
+    plain_text = f"""Hi {username},
+
+    You requested to reset your password. Click the link below to set a new password:
+
+    {reset_url}
+
+    This link will expire in 1 hour.
+
+    If you didn't request this, you can safely ignore this email.
+
+    Best regards,
+    The FastAPI Blog Team
+    """
+
+    await send_email(
+        to_email=to_email,
+        subject="Reset Your Password - FastAPI Blog",
+        plain_text=plain_text,
+        html_content=html_content,
+    )
+
+
     
+
 
